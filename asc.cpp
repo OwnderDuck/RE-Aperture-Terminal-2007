5dca9bf34afe4aafde679ffb694ae01fa2858eaf
OwnderDuck <ownderduck@gmail.com>
2026-08-18 11:00:52 +0800

    Remaining tasks: Implement APPLY and NOTE.
    动画和链接还不知道怎么处理

---
+++ asc.cpp
@@ -0,0 +1,203 @@
+#include <bits/stdc++.h>
+#include <termios.h>
+using namespace std;
+
+termios oldTerm;
+void rawMode(bool enable){
+    if (enable) {
+        tcgetattr(STDIN_FILENO, &oldTerm);
+        termios t = oldTerm;
+        t.c_lflag &= ~(ICANON | ECHO);
+        t.c_cc[VMIN] = 1;
+        t.c_cc[VTIME] = 0;
+        tcsetattr(STDIN_FILENO, TCSANOW, &t);
+    } else {
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldTerm);
+    }
+}
+struct RawMode {
+    RawMode()  { rawMode(1); }
+    ~RawMode() { rawMode(0); }
+};
+
+int keepRunning=0;
+enum stateEnum {UNLOGIN,USERNAME,PASSWORD,LOGINED,CAKE};
+string input;
+void typeString(string s,int delay) {
+    for (auto x:s) {
+        if (delay>0) usleep(delay*1000);
+        putchar(x);
+        fflush(stdout);
+    }
+}
+string getLine() {
+    char g;
+    string ans;
+    while (1) {
+        read(STDIN_FILENO,&g, 1);
+        if (g=='\n'){break;}
+        if (g==127||g==8) {if (!ans.empty()) {ans.pop_back();cout<<"\b \b";fflush(stdout);}continue;}
+        if ('a'<=g&&g<='z'){g-=('a'-'A');}
+        ans+=g;
+        cout<<g;
+        fflush(stdout);
+    }
+    return ans;
+}
+// 读取主板的uuid，hash后作为uid的seed
+string getProductUuid(){// 这个要跨平台
+    ifstream file("/sys/class/dmi/id/product_uuid");
+    string uuid;
+    file>>uuid;
+    return uuid;
+}
+uint64_t hashUuid(const string& s){
+    uint64_t h=14695981039346656037ULL;
+    for (char c:s) {
+        h^=(unsigned char)c;
+        h*=1099511628211ULL;
+    }
+    return h;
+}
+string getLocalUid(){
+    mt19937_64 gen(hashUuid(getProductUuid()));
+    ostringstream ss;
+    for (int i=0;i<4;i++) {ss<<hex<<setw(16)<<setfill('0')<<gen();}
+    return ss.str();
+}
+
+int main(){
+    string uid=getLocalUid();
+    keepRunning=1;
+    stateEnum state=UNLOGIN;
+    string username,password;
+
+    rawMode(1);
+    printf("\033[2J\033[H");
+    fflush(stdout);
+    char g;
+    bool passwordWrong=0;
+    bool bossKeyMode=0;
+    string hearder="GLaDOS v1.07 (c) 1982 Aperture Science, Inc.",message="",pormpt="B:\\>";
+    while(keepRunning) {
+        input="";
+        switch (state) {
+            case UNLOGIN:
+                printf("\033[2J\033[H");//clr
+                typeString("> ",0);
+                input=getLine();
+                fflush(stdout);
+                if (input=="LOGIN") {state=USERNAME;}
+                else if (input=="HELP") {printf("\033[2J\033[H");typeString("If this is an actual plea for help in response to a hazardous material spill, an explosion, a fire on your person, radiation poisoning, a choking gas of unknown origin, eye trauma resulting from the use of an emergency eye wash station on floors three, four, or eleven, an animal malfunction, or any other injurious experimental equipment failure, please remain at your workstation. A Crisis Response Team has already been mobilized to deliberate on a response to your crisis.\n\nIf you need help accessing the system, please refer to your User Handbook.\n\n",10);}
+                break;
+            case USERNAME:
+                printf("\033[2J\033[H");
+                typeString("Username> ",75);
+                username=getLine();
+                if (username.length()>2) {state=PASSWORD;}
+                break;
+            case PASSWORD:
+                printf("\033[2J\033[H");
+                if (!passwordWrong) {typeString("Password> ",75);}
+                else {typeString("ERROR 07 [Incorrect Password]\n\nPassword> ",25);}
+                g=0;
+                while (1) {
+                    read(STDIN_FILENO,&g, 1);
+                    if (g=='\n'){break;}
+                    if (g==127||g==8) {if (!password.empty()) {password.pop_back();cout<<"\b \b";fflush(stdout);}continue;}
+                    if ('a'<=g&&g<='z'){g-=('a'-'A');}
+                    password+=g;
+                    cout<<'*';
+                    fflush(stdout);
+                }
+                if (username=="CJOHNSON") {
+                    if (password=="TIER3") {hearder="GLaDOS v1.07a (c) 1982 Aperture Science, Inc.";pormpt="ADMIN> ";state=LOGINED;}
+                    else {passwordWrong=1;}
+                } else {
+                    if (password=="PORTAL"||password=="PORTALS") {state=LOGINED;}
+                    else {passwordWrong=1;}
+                }
+                break;
+            case LOGINED:
+                printf("\033[2J\033[H");
+                typeString("GLaDOS v1.07a (c) 1982 Aperture Science, Inc."+message+"\n\n"+pormpt,7);
+                input=getLine();
+                if (input=="THECAKEISALIE") {state=CAKE;}
+                else if (input=="DIR"||input=="CATALOG"||input=="DIRECTORY"||input=="LIST"||input=="LS"||input=="CAT") {if (username=="CJOHNSON") {message="                                                                                                                \n                                                                                                                                                                                                                      \nDISK VOLUME 255 [WORKSTATION CJOHNSON]\n\n     I  019  APPLY.EXE\n     I  004  NOTES.EXE\n\n2 FILE(S) IN 23 BLOCKS\n\n";} else {message="                                                                                                                \n                                                                                                                                                                                                                      \nDISK VOLUME 255 [NEW EMPLOYEE WORKSTATION]\n\n     I  019  APPLY.EXE\n\n1 FILE(S) IN 19 BLOCKS\n\n";}}
+                else if (input=="IP") {message=" \n\nuid:"+uid+"\n";}
+                else if (input=="HELP"||input=="LIB"||input=="?") {if(username=="CJOHNSON") {message=" \n\nLIB\n     NOTES\n     APPEND\n     ATTRIB\n     COPY\n     DIR\n     ERASE\n     FORMAT\n     INTERROGATE\n     LIB\n     PLAY\n     RENAME\n     TAPEDISK";} else {message = " \n\nLIB\n     APPEND\n     ATTRIB\n     COPY\n     DIR\n     ERASE\n     FORMAT\n     INTERROGATE\n     LIB\n     PLAY\n     RENAME\n     TAPEDISK";}}
+                else if (input=="LOGOUT"||input=="BYE"||input=="LOGOFF"||input=="VALUE") {/*open steam*/;state=UNLOGIN;}
+                else if (input=="APPEND"||input=="ATTRIB"||input=="COPY"||input=="FORMAT"||input=="ERASE"||input=="RENAME") {message = "                                                                                                                \n                                                                                                                                                                                                                      \nERROR 15 [Disk is write protected]";}
+                else if (input=="PLAY") {message="\n\nERROR 03 [What would you like to play?]";}
+                else if (input=="PLAY PORTAL") {/*PORTAL!*/;state=LOGINED;}
+                else if (input=="INTERROGATE") {message="\n\nERROR 02 [Command requires at least one parameter]";}
+                else if (input[0]=='I'&&input[1]=='N'&&input[2]=='T'&&input[3]=='E'&&input[4]=='R'&&input[5]=='R'&&input[6]=='O'&&input[7]=='G'&&input[8]=='A'&&input[9]=='T'&&input[10]=='E') {if (username=="CJOHNSON") {message = "\n\nERROR 07 [Unknown Employee]";}else {message = "\n\nERROR 01 [Illegal attempt to initiate disciplinary action]";}}
+                else {message="\n\nERROR 24 [File \'"+input+"\' not found]";}
+                break;
+            case CAKE:
+                if (!bossKeyMode) {
+                    printf("\033[2J\033[H");
+                    printf( ">\n"
+                            ">>>&!>>\n"
+                            "When was the last time you left the building?\n"
+                            "Has anybody left the building lately?\n"
+                            "I don\'t know why we\'re in lockdown. I don\'t know who\'s in charge.\n"
+                            "I did find out a few things, like these terminals don\'t have to\n"
+                            "tap out characters one at a time. And while we\'re all working\n"
+                            "on twenty year old equipment, somehow they can afford to build\n"
+                            "an \'Enrichment Center\'. Check out this security feed.\n"
+                            "Whatever the hell a \'relaxation vault\' is, it\n"
+                            "doesn\'t have any doors.\n\n\n\n\n\n"
+                            "I don\'t think going home is part of our job description anymore.\n\n"
+                            "If a supervisor walks by, press return!\n");
+                    g=0;
+                    while(1){
+                        read(STDIN_FILENO,&g,1);
+                        if(g) {
+                            bossKeyMode=1;
+                            break;
+                        }
+                    }
+                } else {
+                    printf("\033[2J\033[H");
+                    printf(
+                        "\033[7mB8    (L) TOTAL\033[47m\033[K\033[0m\n"
+                        "\033[47m\033[K\033[0m\n"
+                        "\033[0m\n"
+                        "\033[7m            A          B          C          D          E\033[47m\033[K\033[0m\n"
+                        "\033[7m     1\033[0m\n"
+                        "\033[7m     2\033[0m     ITEM      UNITS      PER-U       EXT\n"
+                        "\033[7m     3\033[0m     ----      -----      -----      ------\n"
+                        "\033[7m     4\033[0m    FLOUR         50      21.50     1075.00\n"
+                        "\033[7m     5\033[0mINTUB-XLG          1 974,999.99  974,999.99\n"
+                        "\033[7m     6\033[0mTACK-THMB         75       0.02        1.50\n"
+                        "\033[7m     7\033[0m                                 ----------\n"
+                        "\033[7m     8\033[0m         \033[7mTOTAL        \033[0m           976,076.49\n"
+                        "\033[7m     9\033[0m\n"
+                        "\033[7m    10\033[0m\n"
+                        "\033[7m    11\033[0m\n"
+                        "\033[7m    12\033[0m\n"
+                        "\033[7m    13\033[0m\n"
+                        "\033[7m    14\033[0m\n"
+                        "\033[7m    15\033[0m\n"
+                        "\033[7m    16\033[0m\n"
+                        "\033[7m    17\033[0m\n"
+                        "\033[7m    18\033[0m\n"
+                        "\033[7m    19\033[0m\n"
+                        "\033[7m    20\033[0m\n"
+                        "\033[7m    21\033[0m\n"
+                       );
+                    fflush(stdout);
+                    while(1){
+                        read(STDIN_FILENO,&g,1);
+                        if(g) {
+                            bossKeyMode=0;
+                            break;
+                        }
+                    }
+                }
+                break;
+
+        }
+    }
+}
